a75e500ba88170cf6e19419deccd19ec188afc05
DukTai <nguyenductai.12a2tqk@gmail.com>
2026-05-27 15:58:51 +0700

    chore(api): set global prefix api/v1 in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Kích hoạt tiền tố toàn cục /api/v1 theo tài liệu đặc tả thiết kế
+  app.setGlobalPrefix('api/v1');
+
   const configService = app.get(ConfigService);
   const port = configService.get<number>('APP_PORT', 3000);
 
