a529a297348b673cd64eaf3d773994cbad2e20fb
Tran Duc Hai <haiductran712@gmail.com>
2026-07-24 09:05:43 +0700

    fix(db): bat ssl cho data-source khi DB_SSL=true (ket noi RDS)

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -30,4 +30,5 @@ export const AppDataSource = new DataSource({
   entities: [path.join(__dirname, '../modules/**/*.entity.{ts,js}')],
   migrations: [path.join(__dirname, './migrations/*.{ts,js}')],
   migrationsTableName: 'typeorm_migrations',
+
 });
